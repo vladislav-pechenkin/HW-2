ffe3c6d13cc2636b3e2942221621a6d6144ae4e3
User <89028637729@mail.ru>
2024-06-19 11:58:38 +0500

    Ввел в консоль новые значения переменных. Уменьшил значения каждой преременной.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
+
         System.out.println(paper);
         dog +=4;
         System.out.println(dog);
@@ -12,5 +13,20 @@ public class Main {
         System.out.println(cat);
         paper +=4;
         System.out.println(paper);
+
+        var nevDog = 8.0;
+        System.out.println(nevDog);
+        var nevCat = 3.6;
+        System.out.println(nevCat);
+        var nevPaper = 763789;
+        System.out.println(nevPaper);
+
+        nevDog -= 3.5;
+        System.out.println(nevDog);
+        nevCat -= 1.6;
+        System.out.println(nevCat);
+        nevPaper -= 7639;
+        System.out.println(nevPaper);
+
     }
 }
